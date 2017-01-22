851c8cdf3071d1a81a8e49d798576fb898f8c22a
grbd <garlicbready@googlemail.com>
2017-01-22 23:16:08 +0000

    cleanups

--- Source/Examples/Logging/TestApp1/Program.cs
+++ Source/Examples/Logging/TestApp1/Program.cs
@@ -3,9 +3,9 @@ using Serilog;
 using TestApp1.Logging;
 
 namespace TestApp1 {
-    class Program {
+    public class Program {
 
-        static void Main(string[] args) {
+        public static void Main(string[] args) {
 
             // Setup Serilog
             Log.Logger = new LoggerConfiguration()
--- Source/Examples/Logging/TestApp2/MyClass.cs
+++ Source/Examples/Logging/TestApp2/MyClass.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestApp2.Logging;
+﻿using TestApp2.Logging;
 
 namespace TestApp2 {
     public class MyClass {
